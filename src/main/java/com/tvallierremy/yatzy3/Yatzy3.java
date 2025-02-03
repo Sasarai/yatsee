62a3580c54985dac28a7cee68a583f43adcd7a6d
Thomas VALLIER-REMY <thomas.vallier-remy-ext@mousquetaires.com>
2025-02-03 18:15:00 +0100

    Delete unnecessary code

--- src/main/java/com/tvallierremy/yatzy3/Yatzy3.java
+++ src/main/java/com/tvallierremy/yatzy3/Yatzy3.java
@@ -7,11 +7,6 @@ import java.util.List;
 
 public class Yatzy3 implements YatzyCalculator {
     @Override
-    public List<String> validCategories() {
-        return Arrays.stream(YatzyCategory.values()).map(Enum::toString).toList();
-    }
-
-    @Override
     public int score(List<Integer> dice, String category) {
         return CategoryScorer.createInstance(category).calculateScore(dice);
     }
--- src/main/java/com/tvallierremy/yatzy3/YatzyCalculator.java
+++ src/main/java/com/tvallierremy/yatzy3/YatzyCalculator.java
@@ -3,6 +3,5 @@ package com.tvallierremy.yatzy3;
 import java.util.List;
 
 public interface YatzyCalculator {
-    List<String> validCategories();
     int score(List<Integer> dice, String category);
 }
